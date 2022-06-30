d978998dbdd1226c0bdacf740020cc039ae9e2bc
Pablo Cezar <102382723+Pablocezar777@users.noreply.github.com>
2022-06-30 00:26:26 -0300

    Update Conversordepagamento.py

--- Conversordepagamento.py
+++ Conversordepagamento.py
@@ -1,13 +1,17 @@
-from os import system
+from time import sleep
+import os
+
+def clear():
+    os.system("cls") #Ou Clear depende muito
 
 print('》》Conversor de pagamento《《')
 print(" ")
 
 while True:
-    system('cls')
     print("---" * 15)
     modo = int(input('''Modo: 1)Mensal 2)Semanal 3)Hora :'''))
     print(" ")
+    clear()
     if modo == 3:
         salario = float(input('》》Digite o valor por hora:'))
         hr = int(input('》》horas trabalhada:'))
@@ -15,6 +19,7 @@ while True:
         salahr = salario * hr
         extra = hrextra * 2
         total = salahr + extra
+        clear()
         print(" ")
         print('Voce recebe R${:.2f} por hora, voce trabalhou {}hrs e fez {} horas extra'.format(salario, hr, hrextra))
         print(" ")
@@ -24,10 +29,13 @@ while True:
 
         cont = int(input('Realizar nova consulta? 1 para sim e 0 para não:'))
         if cont == 1:
+            clear()
             continue
         else:
             print("---" * 15)
             print('Sistema encerrado!!!')
+            sleep(2)
+            clear()
             break
 
     if modo == 1:
@@ -36,16 +44,20 @@ while True:
         dia = salario * 24
         hrextra = extra * 2
         total = dia + hrextra
+        clear()
         print(" ")
         print('Você recebe R${:.2f} por dia, fez {} hora extra'.format(salario, extra))
         print('Seu salário atualizado é R${:.2f}'.format(total))
 
         cont = int(input('Realizar nova consulta? 1 para sim 0 e para não:'))
         if cont == 1:
+            clear()
             continue
         else:
             print("---" * 15)
             print('Sistema encerrado!!!')
+            sleep(2)
+            clear()
             break
 
     if modo == 2:
@@ -54,20 +66,26 @@ while True:
         sem = salario * 5
         hrextra = extra * 2
         total = sem + hrextra
+        clear()
         print(" ")
         print('Você recebe R${:.2f} por dia e fez {} hora extra'.format(salario, hrextra))
         print('Seu salario atualizado é R${:.2f}'.format(total))
 
         cont = int(input('Realizar nova consulta? 1 para sim e 0 para não:'))
         if cont == 1:
+            clear()
             continue
         else:
             print("---" * 15)
             print('Sistema encerrado!!!')
+            sleep(2)
+            clear()
             break
 
     else:
         print('Código Inválido! tente novamente.')
         print("---" * 15)
         print(" ")
+        sleep(2)
+        clear()
         continue
